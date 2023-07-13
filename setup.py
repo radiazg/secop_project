aa4cf41f358207054f264dfbe1ea06cc2b169681
Ricardo Diaz <ing.rdiaz@gmail.com>
2023-07-12 21:41:04 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data Science project using a SECOP Dataset',
+    author='Ricardo Diaz, Santiago Castellanos',
+    license='MIT',
+)
